cd7c54557be1e48956936209d816f513c3dd8d7e
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2019-07-30 02:16:26 +0700

    [UPDATE] url auth [ADD] socialite config in .env.example

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::view('/', 'landing-page');
 Route::view('catalog', 'our-catalog');
 Route::view('face-result', 'face-result');
+Route::get('auth', 'Auth\LoginController@showLoginForm')->name('login');
 Route::get('login/{provider}', 'Auth\LoginController@redirectToProvider')->name('login.provider');
 Route::get('login/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('login.callback');
