3eb9949ab261af3671aa290fa10cf97d931853f9
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2019-07-30 18:08:04 +0700

    [ADD] catalog selected page design

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::view('/', 'landing-page');
-Route::view('catalog', 'our-catalog');
+Route::view('catalog', 'catalog.default');
 Route::view('face-result', 'face-result');
+Route::view('catalog/selected', 'catalog.selected');
 Route::get('auth', 'Auth\LoginController@showLoginForm')->name('login');
 Route::get('login/{provider}', 'Auth\LoginController@redirectToProvider')->name('login.provider');
 Route::get('login/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('login.callback');
