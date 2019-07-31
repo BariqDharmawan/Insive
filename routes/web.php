1bd0cee177bd334d109afb44802cc310a44a79eb
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2019-07-31 15:00:23 +0700

    [FIX] increase and decrease number catalog

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::view('/', 'landing-page');
 Route::view('catalog', 'catalog.default');
 Route::view('face-result', 'face-result');
 Route::view('catalog/selected', 'catalog.selected');
+Route::view('custom/sheet', 'custom.sheet');
+Route::view('contact-us', 'contact');
 Route::get('auth', 'Auth\LoginController@showLoginForm')->name('login');
 Route::get('login/{provider}', 'Auth\LoginController@redirectToProvider')->name('login.provider');
 Route::get('login/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('login.callback');
