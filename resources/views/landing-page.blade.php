18217e6a5a15263c5bbf8ceefedcf9c1b5108933
Bariq Dharmawan <bariq.2nd.rodriguez@gmail.com>
2019-08-06 23:59:37 +0700

    [FIX] landing page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -16,33 +16,23 @@
           <img src="{{ asset('img/muka/skin_oily.png') }}" height="400" alt="Slideshow Image">
         </div>
       </div>
-      <a class="carousel-control-prev" href="#landing-logo-carousel" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#landing-logo-carousel" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
     @if (Auth::check())
-    <h4 class="text-white text-capitalize text-center">Welcome, {{Auth::user()->name}}</h4>
     @endif
     <div class="landing-link">
-      @if (Auth::check())
-      <a href="{{ route('main.question') }}">
-        <span>Start Quiz <i class='bx bx-log-in-circle'></i></span>
-      </a>
-      @else
-      <a href="{{ route('login') }}">
-        <span><i class='bx bx-user-plus'></i>Register</span>
-        <span>or</span>
-        <span>Login <i class='bx bx-log-in-circle'></i></span>
-      </a>
-      @endif
+      @auth
+        <h4 class="text-white text-capitalize text-center">Welcome, {{Auth::user()->name}}</h4>
+      @endauth
+      @guest
+        <a href="{{ route('login') }}">
+          <span><i class='bx bx-user-plus'></i>Register</span>
+          <span>or</span>
+          <span>Login <i class='bx bx-log-in-circle'></i></span>
+        </a>
+      @endguest
     </div>
     <div class="recommend-link">
-      <a href="">Custom your SHEET MASK!</a>
+      <a href="{{ route('main.question') }}">Custom your SHEET MASK!</a>
       <small>or</small>
       <a href="{{ route('catalog.default') }}">buy from our catalog!</a>
     </div>
