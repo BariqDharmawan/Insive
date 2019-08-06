dc5cd0da846ba4feb14ba7b7ebe0bf25a3ac1a0a
Bariq Dharmawan <bariq.2nd.rodriguez@gmail.com>
2019-08-06 23:07:41 +0700

    [ADD] comment for better readable

--- public/js/native.js
+++ public/js/native.js
@@ -11,10 +11,10 @@ $(document).ready(function(){
   if (window.location.pathname === '/') {
     $("body").attr("id", "landing-page");
   }
-  else if (window.location.href.indexOf('catalog/selected') > -1) {
+  else if (window.location.href.indexOf('catalog/selected') > -1) { //jika url catalog/selected/{parameter}
     $("body").attr("id", "catalog-selected-page");
   }
-  else if (window.location.href.indexOf('custom/package') > -1) {
+  else if (window.location.href.indexOf('custom/package') > -1) { //jika url custom/package/{parameter}
     $("body").attr("id", 'custom-package-page');
   }
   else {
