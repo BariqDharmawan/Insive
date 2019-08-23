0a31169ca2a363cfdec8be096e4837ff90fae82a
Bariq Dharmawan <bariq.2nd.rodriguez@gmail.com>
2019-08-23 17:43:06 +0700

    [ADD] pagination on catalog

--- resources/views/catalog/default.blade.php
+++ resources/views/catalog/default.blade.php
@@ -140,6 +140,24 @@
           </figure>
         </div>
       </div>
+      <div class="row mx-0">
+        <ul class="pagination col-12">
+          <li class="page-item disabled">
+            <span class="page-link">Previous</span>
+          </li>
+          <li class="page-item active"><a class="page-link" href="#">1</a></li>
+          <li class="page-item" aria-current="page">
+            <span class="page-link">
+              2
+              <span class="sr-only">(current)</span>
+            </span>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </main>
 @endsection
