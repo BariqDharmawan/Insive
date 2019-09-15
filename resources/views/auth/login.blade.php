bf1034fb4ba2fe2646a1c9db470466eb6b502c9f
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2019-09-15 17:14:19 +0700

    nothing

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Auth</title>
+    <title>Login With Your Account</title>
     <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
     <link href='https://unpkg.com/boxicons@2.0.2/css/boxicons.min.css' rel='stylesheet'>
   </head>
