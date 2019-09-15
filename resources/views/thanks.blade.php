8d9bc60ac9c83526404a33ab34fb67e9508e5313
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2019-09-15 23:48:27 +0700

    [ADD] continue shopping

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -3,24 +3,34 @@
 @section('css')
   <style media="screen">
     main {
-      height: calc(100vh - 110px - 60px - 65px);
-    }
-    main .container {
-      height: 100%;
       display: flex;
       align-items: center;
-      justify-content: center;
+      height: calc(100vh - 110px - 60px - 65px);
     }
     main h1 strong {
       text-transform: uppercase;
       text-decoration: underline;
+    }
+    main h4:hover {
+      color: #E2CCC1 !important;
     }
   </style>
 @endsection
 @section('content')
   <main>
     <div class="container">
-      <h1 class="text-capitalize text--cream">thanks <strong>{{ Auth::user()->name }}</strong> for your ordering!​</h1>
+      <div class="row justify-content-center mb-3">
+        <h1 class="text-capitalize text--cream mb-4 w-100 text-center">thanks <strong>{{ Auth::user()->name }}</strong> for your ordering!​</h1>
+        <h4 class="text--cream">Let's Continue Shopping!</h4>
+      </div>
+      <div class="row justify-content-center mb-4">
+        <a href="{{ route('main.question') }}" class="btn bg--cream">Yapp, I wanna Custom Mask Again</a>
+        <span class="text-secondary d-inline-flex align-items-center mx-4">or</span>
+        <a href="{{ route('catalog.default') }}" class="btn bg--cream">Sure, Take Me To The Catalog</a>
+      </div>
+      <div class="row justify-content-center">
+        <a href="{{ url('/') }}" class="btn text-capitalize btn-link text--cream">Nope, I just wanna to homepage</a>
+      </div>
     </div>
   </main>
 @endsection
