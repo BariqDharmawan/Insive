f58167882607e9a6293672f1b7270092eccc51d2
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-10-17 16:29:12 +0700

    [UPDATE] product dummy record

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'product_name' => $faker->unique()->randomElement([
+            'Go Away Acne!', 
+            'Hydrate Me!', 
+            'Whitening is Coming!', 
+            'Korean Choc-choc!', 
+            'Japan’s Pitera!'
+        ]),
+        'price' => $faker->randomElement([19900, 20900, 39999]),
+        'qty' => random_int(1, 99),
+        'type' => $faker->randomElement(['a', 'b', 'c']),
+        'category' => 'mask'
+    ];
+});
