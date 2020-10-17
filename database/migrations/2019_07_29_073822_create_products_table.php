c3d2f54feab16eb4efbcef766fe0df68ef8b66de
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-10-17 17:06:46 +0700

    [ADD] product name validation

--- database/migrations/2019_07_29_073822_create_products_table.php
+++ database/migrations/2019_07_29_073822_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('product_name', 100);
+            $table->string('product_name', 100)->unique();
             $table->integer('price');
             $table->integer('qty');
             $table->string('type');
