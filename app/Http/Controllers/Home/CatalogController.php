33e846ee6d817cf9823a284b9d3a141e8fbe227e
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-10-25 17:53:49 +0700

    [ADD] final price on catalog

--- app/Http/Controllers/Home/CatalogController.php
+++ app/Http/Controllers/Home/CatalogController.php
@@ -2,24 +2,28 @@
 
 namespace App\Http\Controllers\Home;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Product;
 use App\Models\SubCart;
-use Auth;
+use Illuminate\Http\Request;
+use App\Models\ProductDiscount;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class CatalogController extends Controller
 {
     public function __construct()
     {
-      $this->middleware(['auth', 'verified']);
+        $this->middleware(['auth', 'verified']);
     }
-    
+
     public function index()
     {
-        $data['product'] = Product::where('qty', '>', 0)->paginate(20);
-        return view('catalog.homepage')->with($data);
+        $product = Product::with('discount')->where('qty', '>', 0)->paginate(20);
+        return view('catalog.homepage', [
+            'product' => $product,
+            'discount' => ProductDiscount::all()
+        ]);
     }
 
     public function store(Request $request)
@@ -28,21 +32,31 @@ class CatalogController extends Controller
             'product_id.*' => ['required'],
             'jumlah_beli.*' => ['required'],
         ]);
-        $user_id = Auth::user()->id;
+        $user_id = Auth::id();
         $code_cart = Cart::orderBy('id', 'desc')->first();
-        if(empty($code_cart)) {
-            $code = 'C'.date('HisYmd').$user_id.sprintf('%05d', 1);
+        if (empty($code_cart)) {
+            $code = 'C' . date('HisYmd') . $user_id . sprintf('%05d', 1);
         } else {
-            $code = 'C'.date('HisYmd').$user_id.sprintf('%05d', substr($code_cart->cart_code, -1)+1);
+            $code = 'C' . date('HisYmd') . $user_id . sprintf('%05d', substr($code_cart->cart_code, -1) + 1);
         }
         $table = Cart::firstOrCreate(
-            ['user_id' => $user_id, 'type_cart' => 'catalog', 'status' => 'waiting'],
-            ['user_id' => $user_id, 'cart_code' => $code, 'formula_code' => '#01234', 'type_cart' => 'catalog', 'status' => 'waiting']
+            [
+                'user_id' => $user_id,
+                'type_cart' => 'catalog',
+                'status' => 'waiting'
+            ],
+            [
+                'user_id' => $user_id,
+                'cart_code' => $code,
+                'formula_code' => '#01234',
+                'type_cart' => 'catalog',
+                'status' => 'waiting'
+            ]
         );
         SubCart::where('cart_id', $table->id)->delete();
         $qty = $request->jumlah_beli;
         foreach ($request->product_id as $key => $value) {
-            if($qty[$key] > 0) {
+            if ($qty[$key] > 0) {
                 $product = Product::where('id', $value)->first();
                 $subCart = new SubCart;
                 $subCart->user_id = $user_id;
@@ -50,7 +64,7 @@ class CatalogController extends Controller
                 $subCart->product_id = $product->id;
                 $subCart->qty = $qty[$key];
                 $subCart->price = $product->price;
-                $subCart->total_price = $qty[$key]*$product->price;
+                $subCart->total_price = $qty[$key] * $product->price;
                 $subCart->save();
             }
         }
@@ -60,6 +74,5 @@ class CatalogController extends Controller
 
     public function cartIndex()
     {
-
     }
 }
