648f6d54e3f7cef1044c67182b8000af2b70d761
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-11-15 14:44:10 +0700

    [UPDATE] check if user already verified or not

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,34 +7,33 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('verified');
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
+    $this->middleware('verified');
+  }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        if (Auth::user()->email_verified_at == '') {
-          return view('home');
-        }
-        else {
-          return redirect()->back();
-        }
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Contracts\Support\Renderable
+   */
+  public function index()
+  {
+    if (!Auth::user()->hasVerifiedEmail()) {
+      return view('home');
+    } else {
+      return redirect()->back();
     }
-    
-    public function storeContact()
-    {
-      // code...
-    }
+  }
+
+  public function storeContact()
+  {
+    // code...
+  }
 }
